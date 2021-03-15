19f9c9a9b126369bcecfaa0c8f0f573b9c4e80cb
Bogdan S <bseleng@gmail.com>
2021-03-15 10:06:57 +0300

    feat: add count-cats solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-
+module.exports = function countCats(array) {
   // remove line with error and write your code here
+  let count = 0;
+  array.forEach((subarray) => {
+    let filtered = subarray.filter((el) => el === "^^");
+    count += filtered.length;
+  });
+
+  return count;
 };
